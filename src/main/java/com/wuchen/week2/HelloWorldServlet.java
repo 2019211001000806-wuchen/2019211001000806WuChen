dcc80e9adebc0dd55256ed7f8a7a9a774c59b454
lenovo <2019211001000806WuChen>
2021-03-13 15:28:25 +0800

    2.HW-CE1 Commit

---
+++ src/main/java/com/wuchen/week2/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package com.wuchen.week2;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        PrintWriter writer= response.getWriter();
+        writer.println("Name:Wu Chen");
+        writer.println("ID:2019211001000806");
+        writer.println("Date and Time:Sat Match 13 15:23:02 CST 2021 ");
+    }
+}
