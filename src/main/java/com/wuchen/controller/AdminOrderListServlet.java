2995625ca0450a7011a8322f21b6b02fa39db341
lenovo <2019211001000806WuChen>
2021-06-12 13:11:32 +0800

    week15

---
+++ src/main/java/com/wuchen/controller/AdminOrderListServlet.java
@@ -0,0 +1,41 @@
+package com.wuchen.controller;
+
+import com.wuchen.dao.OrderDao;
+import com.wuchen.model.Item;
+import com.wuchen.model.Order;
+import com.wuchen.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "AdminOrderListServlet", value = "/admin/orderList")
+public class AdminOrderListServlet extends HttpServlet {
+    private Connection con=null;
+    public  void init() throws ServletException{
+        con=(Connection)getServletContext().getAttribute("con");
+    }
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Payment> paymentTypeList=Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao=new OrderDao();
+        List<Order> orderList=orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path="/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    doGet(request,response);
+    }
+}
